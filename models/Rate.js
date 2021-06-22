ebe99a2f49ce09206a489da7c81c5cb70d59c27d
Fredric McWilliams <fredmcwilliams92@gmail.com>
2021-06-21 19:41:52 -0500

    added in the verbiage for the rate file

--- models/Rate.js
+++ models/Rate.js
@@ -4,3 +4,38 @@ const sequelize = require('../config/connections');
 class Rate extends Model {}
 
 Rate.init({})
+
+Rate.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'vote'
+  }
+);
+
+module.exports = Rate;
