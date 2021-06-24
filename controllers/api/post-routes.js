9e93308f4263379bf857284c60fc03773297f306
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-06-23 19:54:51 -0500

    resaved

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -89,7 +89,7 @@ router.post('/', (req, res) => {
 
 router.put('/upvote', (req, res) => {
     // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
+    Post.upvote(req.body, { Rate })
         .then(updatedPostData => res.json(updatedPostData))
         .catch(err => {
             console.log(err);
