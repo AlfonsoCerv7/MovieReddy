dd5a47f18f66bbd6330a657d89143d2dee673730
Archita Racherla <archita.racherla@gmail.com>
2021-06-23 19:54:42 -0500

    homepage, stylesheet, and npm package

--- models/Rate.js
+++ models/Rate.js
@@ -1,40 +1,38 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connections');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connections");
 
 class Rate extends Model {}
-
-//Rate.init({})
 
 Rate.init(
   {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'user',
-        key: 'id'
-      }
+        model: "user",
+        key: "id",
+      },
     },
     post_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'post',
-        key: 'id'
-      }
-    }
+        model: "post",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'vote'
+    modelName: "rate",
   }
 );
 
