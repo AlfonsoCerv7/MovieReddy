2b4539739ba0d2e19616c80663238b30f8c6c669
Marcos <marcosbadillo089@gmail.com>
2021-06-23 21:23:36 -0500

    got fetch api to display image example

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,15 @@
+const APIKey = '141d524e5cf007818feee1b4ecf58351';
+ const url = 'https://api.themoviedb.org/3/search/movie?api_key=';
+ let userInput = '';
 
-APIKey = '141d524e5cf007818feee1b4ecf58351';
+//Moviedb documentation to get image you need:
+//To build an image url I need 1)base_url 2)file_size 3)path_size
 
-fetch('https://api.themoviedb.org/3/movie/550?api_key=' + APIKey
-  )
-    // Converts the response to JSON
-    .then(function(response) {
+ //this first fetch gets the base_url
+//fetch('https://api.themoviedb.org/3/configuration?api_key=' + APIKey
+fetch( url + APIKey + '&query=Jack+Reacher'
+)
+ .then(function(response) {
       return response.json();
     })
     .then(function(response) {
@@ -16,8 +21,11 @@ fetch('https://api.themoviedb.org/3/movie/550?api_key=' + APIKey
         var imgEl = document.createElement("img");
         
       // Set that element's 'src' attribute to the 'image_url' from our moviedb API response
-      imgEl.setAttribute('src', response.data.original_title); //get data from console log under data
-  
-      // Append the '<img>' element to the page
+      imgEl.setAttribute('src',`https://image.tmdb.org/t/p/w500${response.results[0].backdrop_path}`); //get response data from console log
+      if(response.length) {
+
+      }
+            // Append the '<img>' element to the page
         moviedbImgae.appendChild(imgEl);
+
     });
