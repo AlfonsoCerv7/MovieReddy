7c47eecd32938f77c4c904d57f7158bd13a8582e
Marcos <marcosbadillo089@gmail.com>
2021-06-28 20:52:30 -0500

    updated the moviefetch code

--- controllers/api/moviedbApiFetch-routes.js
+++ controllers/api/moviedbApiFetch-routes.js
@@ -6,13 +6,12 @@ const movieButton = document.getElementById("movie-button");
 movieButton.addEventListener("click", function(){
  const movieInput = document.getElementById("movie-input").value;
 
-//create an if statement to delete imgEl (movie poster) and overviewEl (movie details) when user searches another movie title
-//
-//
+//create an if statement to delete/remove imgEl (movie poster), overviewEl (movie details), etc when user searches another movie title
+
 
 console.log('log: movieInput ', movieInput);
 
-//this will fetch the moviedb api and has the user input to ouput the movie image of their choosing
+//this will fetch the moviedb api and has the user input to ouput the movie image, release date, movie detail of their choosing
 fetch( url + APIKey + '&query=' + movieInput
 )
 .then(function(response) {
@@ -20,40 +19,50 @@ fetch( url + APIKey + '&query=' + movieInput
    })
    .then(function(response) {
      console.log(response);
-
+     
 //=============================This gets the moviedb Image========================================================//
 
-     // Use 'querySelector' to get the ID of where the moviedb image will be displayed
-      var moviedbImage = document.querySelector('#moviedb-image');
+   // Use 'querySelector' to get the ID of where the moviedb image will be displayed
+   var moviedbImage = document.querySelector('#moviedb-image');
  
-     // Create an '<img>' element
-       var imgEl = document.createElement("img");
+   // Create an '<img>' element
+   var imgEl = document.createElement("img");
        
-     // Set that element's 'src' attribute to the 'image_url' from the moviedb API response
-     imgEl.setAttribute('src',`https://image.tmdb.org/t/p/w500${response.results[0].poster_path}`); //get response data from console log
+   // Set that element's 'src' attribute to the 'image_url' from the moviedb API response
+   imgEl.setAttribute('src',`https://image.tmdb.org/t/p/w500${response.results[0].poster_path}`); //get response data from console log
      
-     // Append the '<img>' element to the page
-     moviedbImage.appendChild(imgEl);
+   // Append the '<img>' element to the page
+   moviedbImage.appendChild(imgEl);
 
 //================================End of the moviedb Image========================================================//
 
+
+//=============================This gets the moviedb release date=================================================//
+
+   //Use getElementById to get the ID where the movie release date will be displayed
+   var movieRelease = document.getElementById('movie-release');
+
+   //create a div element from release_date and have it appear in the index.html
+   var movieDate = document.createElement("div").innerHTML = response.results[0].release_date;
+
+   //Append the api response release date
+   movieRelease.append(movieDate);
+
+//===============================End of the moviedb release date==================================================//
+
+
 //===============================This gets the movie detail ======================================================//
 
-     // Use 'querySelector' to get the ID of where the movie detail will be displayed
-     var movieDetail = document.querySelector('#movie-detail');
+   // Use getElementById to get the ID of where the movie detail will be displayed
+   var movieDetail = document.getElementById('movie-detail');
 
-     //create an '<overview>' element
-       var overviewEl = document.createElement("overview");
+   //create a div element for movieOverview and have it appear in the index.html
+   var movieOverview = document.createElement("div").innerHTML = response.results[0].overview;
 
-     // Set that element's 'src' attribute to the image_url from the moviedb API response and target the overview to display the movie details
-     overviewEl.setAttribute('src',`https://image.tmdb.org/t/p/w500${response.results[0].overview}`); //get response data from console log
+   //Append the api response overview
+   movieDetail.append(movieOverview);
 
-     //Append the '<overview>' element to the page
-     movieDetail.appendChild(overviewEl);
-
-//===============================End of the movie detail =======================================================//
+//===============================End of the movie detail ========================================================//
  });
 })
-
-
 
