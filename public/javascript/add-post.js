3d33db9c8cbb53ab101c79f88da70977886cfe59
Archita Racherla <archita.racherla@gmail.com>
2021-06-29 22:52:39 -0500

    edit, comments, minor fixes

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -24,12 +24,17 @@ window.onclick = function (event) {
   }
 };
 
-const star_rating = document.querySelector('input[name="rate"]').value;
-console.log("This is the star: " + star_rating);
+//const star_rating = document.querySelector('input[name="rate"]').value;
+//console.log("This is the star: " + star_rating);
+
+// async function starRating(star_rating) {
+//   return star_rating;
+// }
 
 async function newFormHandler(event) {
+  //const star_rating = await starRating;
   event.preventDefault();
-
+  //console.log(star_rating);
   const title = document.querySelector('input[name="post-title"]').value;
   const movie_review = document.querySelector(
     'input[name="movie-review"]'
@@ -37,9 +42,28 @@ async function newFormHandler(event) {
   const star_rating = document.querySelector('input[name="rate"]').value;
   const genreSelection = document.getElementById("genres");
   const genre = genreSelection.options[genreSelection.selectedIndex].value;
+  // var star_rating;
+  // star_rating = document
+  //   .querySelector("#star5")
+  //   .addEventListener("click", function () {
+  //     return 5;
+  //   });
+  // star_rating = document
+  //   .querySelector("#star4")
+  //   .addEventListener("click", function () {
+  //     return 4;
+  //   });
+  // document.querySelector("#star3").addEventListener("click", function () {
+  //   star_rating = 3;
+  // });
 
+  // document.querySelector("#star2").addEventListener("click", function () {
+  //   star_rating = 2;
+  // });
+  // document.querySelector("#star1").addEventListener("click", function () {
+  //   star_rating = 1;
+  // });
   console.log("This is the star: " + star_rating);
-
   const response = await fetch(`/api/posts`, {
     method: "POST",
     body: JSON.stringify({
@@ -63,3 +87,93 @@ async function newFormHandler(event) {
 document
   .querySelector(".new-post-form")
   .addEventListener("submit", newFormHandler);
+
+// document.querySelector("#star5").addEventListener("click", function () {
+//   star_rating = 5;
+//   // starRating(star_rating);
+//   console.log(star_rating);
+// });
+// document.querySelector("#star4").addEventListener("click", function () {
+//   star_rating = 4;
+//   //starRating(star_rating);
+//   console.log(star_rating);
+// });
+// document.querySelector("#star3").addEventListener("click", function () {
+//   star_rating = 3;
+//   //starRating(star_rating);
+//   console.log(star_rating);
+// });
+
+// document.querySelector("#star2").addEventListener("click", function () {
+//   star_rating = 2;
+//   //starRating(star_rating);
+//   console.log(star_rating);
+// });
+// document.querySelector("#star1").addEventListener("click", function () {
+//   star_rating = 1;
+//   // starRating(star_rating);
+//   console.log(star_rating);
+// });
+
+/*document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", async function (event) {
+    var star_rating = document.querySelector('input[name="rate"]');
+    star_rating.addEventListener(
+      "click",
+      function (event) {
+        var result = getPosition(event);
+
+        // result is your return value
+      },
+      false
+    );
+
+    //const star_rating = await starRating;
+    event.preventDefault();
+    //console.log(star_rating);
+    const title = document.querySelector('input[name="post-title"]').value;
+    const movie_review = document.querySelector(
+      'input[name="movie-review"]'
+    ).value;
+    //const star_rating = document.querySelector('input[name="rate"]').value;
+    const genreSelection = document.getElementById("genres");
+    const genre = genreSelection.options[genreSelection.selectedIndex].value;
+
+    // document.querySelector("#star5").addEventListener("click", function () {
+    //   star_rating = 5;
+    // });
+    // document.querySelector("#star4").addEventListener("click", function () {
+    //   star_rating = 4;
+    // });
+    // document.querySelector("#star3").addEventListener("click", function () {
+    //   star_rating = 3;
+    // });
+
+    // document.querySelector("#star2").addEventListener("click", function () {
+    //   star_rating = 2;
+    // });
+    // document.querySelector("#star1").addEventListener("click", function () {
+    //   star_rating = 1;
+    // });
+    console.log("This is the star: " + star_rating);
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        movie_review,
+        star_rating,
+        genre,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard/");
+    } else {
+      alert(response.statusText);
+    }
+  });
+*/
