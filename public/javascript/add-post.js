6bbd8a795cf08e9355d9ddbd21d363a7f94a5d72
Alfonso Cerv <pedrocerv1@hotmail.com>
2021-06-30 02:16:03 -0500

    initial test of full new modal system

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -3,22 +3,86 @@ $(document).ready(function () {
 });
 
 var main = document
-   .getElementById("moreInfo") // this has to be the name of the button's id
+   .getElementById("myBtn") // this has to be the name of the button's id
    .addEventListener("click", function () {
-        var movieTitle = document.getElementById("post-title").value; // this needs to grab the movie's title to pass it into a var that will then be searched
-        var movieReview = document.getElementById("movie-review").value;
-        var genres = document.getElementById("genres").value
-if (movieTitle == "" || movieReview == "" || value == "default" ) {
-   M.toast({ html: "Please fill in the complete form!" });
-} else{
-   $(".modal").modal({dismissible: false});
-
-}
+$(".modal").modal({dismissible: false});
 });
 
 
 
 
+// code for closing the modal, and clearing it. 
+document.getElementById("post").addEventListener("click", function () {
+  var movieTitle = document.getElementById("post-title").value; // this needs to grab the movie's title to pass it into a var that will then be searched
+  var movieReview = document.getElementById("movie-review").value;
+  var genres = document.getElementById("genres").value
+if (movieTitle == "" || movieReview == "" || genres == "default" ) {
+M.toast({ html: "Please fill in the complete form!" });
+} else{
+//full post syntax will go here(aka old code)
+async function newFormHandler(event) {
+  //const star_rating = await starRating;
+  event.preventDefault();
+  //console.log(star_rating);
+  const title = document.querySelector('input[name="post-title"]').value;
+  const movie_review = document.querySelector(
+    'input[name="movie-review"]'
+  ).value;
+  const star_rating = document.querySelector('input[name="rate"]').value;
+  const genreSelection = document.getElementById("genres");
+  const genre = genreSelection.options[genreSelection.selectedIndex].value;
+  // var star_rating;
+  // star_rating = document
+  //   .querySelector("#star5")
+  //   .addEventListener("click", function () {
+  //     return 5;
+  //   });
+  // star_rating = document
+  //   .querySelector("#star4")
+  //   .addEventListener("click", function () {
+  //     return 4;
+  //   });
+  // document.querySelector("#star3").addEventListener("click", function () {
+  //   star_rating = 3;
+  // });
+
+  // document.querySelector("#star2").addEventListener("click", function () {
+  //   star_rating = 2;
+  // });
+  // document.querySelector("#star1").addEventListener("click", function () {
+  //   star_rating = 1;
+  // });
+  console.log("This is the star: " + star_rating);
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      movie_review,
+      star_rating,
+      genre,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }); 
+
+  if (response.ok) {
+    document.location.replace("/dashboard/");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .getElementById("reviewSubmission") // this will be changed to getelementbyid(post)
+  .addEventListener("submit", newFormHandler);
+
+
+}
+// revisit clearing text input after successful posting later
+// movieTitle.innerHTML="";
+// movieReview.innerHTML="";
+});
 
 
 
